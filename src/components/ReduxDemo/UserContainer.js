49e462ee0a3b628e32eb8e6dd6a7aaa813d83ac5
hardi shah <hardishahhardi@gmail.com>
2021-01-29 18:53:46 +0530

    Initial Commit

--- src/components/ReduxDemo/UserContainer.js
+++ src/components/ReduxDemo/UserContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { connect} from 'react-redux'
 import { fetchUsers } from '../../redux'
 
 function UserContainer({ userData, fetchUsers }) {
